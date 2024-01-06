2d65081993417e6579f82b825fe18bd68a075004
Picorovisck <ploglamador@hotmail.com>
2024-01-06 07:54:26 -0300

    create-store done

---
+++ src/controller/CreateStore.ts
@@ -0,0 +1,21 @@
+import {Request,Response} from 'express'
+
+
+export class CreateStore{
+  async handle(req:Request,res:Response){
+    const {owner,products,price,amount} = req.body;
+    
+    const user = {
+      owner,
+      products,
+      price,
+      amount
+    } 
+
+    if(!user){
+      res.status(405).send({message: "ERROR ao criar a loja. Verifique os dados."})
+    }
+
+    res.json(user)
+  }
+}
